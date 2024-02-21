f9beb7f9a40144bad0da63c4f77cf13118da65a2
ShannonNCM <61364989+ShannonNCM@users.noreply.github.com>
2024-02-21 10:27:23 -0600

    new commit
    Adecuacion del script a las bases de datos que provienen del sistema

--- funciones_ipc.R
+++ funciones_ipc.R
@@ -10,12 +10,11 @@ cod_grupos <- function(x){
 }
 
 
-
 #Funcion para obtener las ponderaciones de las distitas categorias
 pondcat <- function(x){
   ponderaciones %>% 
     filter(tipo_grupo == x) %>% 
-    select(region_id, grupo_codigo, grupo_nombre, ponderacion_region, indice_grupo) %>% 
+    select(region_id, grupo_codigo, grupo_nombre, ponderacion_region, indice_anterior) %>% 
     rename(region = region_id) %>% 
     mutate(grupo_codigo = as.character(grupo_codigo))
 } 
@@ -25,55 +24,3 @@ variacion <- function(x){
   data.frame(x %>% 
               mutate(var_prod = (precio_actual/cantidad_actual)*(cantidad_anterior/precio_anterior)))
 }
-
-
-
-pondant <- function(x,y){
-  x %>% 
-    filter(tipo_grupo == y) %>% 
-    select(region_id, grupo_codigo, grupo_nombre, ponderacion_region, indice_grupo) %>% 
-    rename(region = region_id) %>% 
-    mutate(grupo_codigo = as.character(grupo_codigo))
-}
-
-
-
-
-
-
-#ESTAS FUNCIONES NO LAS ESTOY USANDO, HAY CAMBIOS PENDIENTES TODAVIA
-
-#funcion para calcular el ipc en las categorias superiores por region
-#x es la columna del dataframe que tiene el codigo del grupo para el que se va a calcular el ipc
-ipc <- function(x, y){
-  data.frame(x %>% 
-               #select(region, y, grupo_nombre, ponderacion_region, ind_prod) %>% 
-               rename(grupo_codigo = y) %>% 
-               group_by(region, grupo_codigo) %>% 
-               summarize(sum1 = sum(ind_prod*ponderacion_region),
-                         sum2 = sum(ponderacion_region)) %>% 
-               mutate(ind = sum1/sum2))
-}
-
-
-
-
-
-#funcion para calcular el ipc en las categorias superiores a nivel nacional
-ipc_nac <- function(x,y){
-  data.frame(x %>% 
-               filter(region != 0) %>% 
-               left_join(y, by ="region") %>%
-               group_by(grupo_codigo, grupo_nombre) %>% 
-               summarize(sum1 = sum(ponderacion_region*participacion), 
-                         sum2 = sum(ponderacion_region*participacion*indice_grupo)) %>% 
-               mutate(ind = sum2/sum1))
-}
-
-
-
-
-
-#funcion para calcular el ipc general
-
-
