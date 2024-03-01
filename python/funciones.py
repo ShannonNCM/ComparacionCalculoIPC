0a0af1c6b6ed2e697ecf737ce1f9f63cc0d3908c
ShannonNCM <61364989+ShannonNCM@users.noreply.github.com>
2024-03-01 11:44:32 -0600

    new update in calculations

---
+++ python/funciones.py
@@ -0,0 +1,10 @@
+#Funciones utilizadas para realizar el calculo
+
+#funcion para asignar los codigos de subclase, clase, grupo y division
+def pondcat(x, ponderaciones):
+    grupo = ponderaciones[ponderaciones['tipo_grupo'] == x]
+    newdata = grupo[['region_id', 'grupo_codigo', 'grupo_nombre', 'ponderacion_region', 'indice_anterior']]
+    newdata = newdata.rename(columns={'region_id': 'region'})
+    newdata['grupo_codigo'] = newdata['grupo_codigo'].astype(str)
+
+    return newdata
