f53ff3fde1995c50696239ffa54fdd0e043bfbc5
ShannonNCM <61364989+ShannonNCM@users.noreply.github.com>
2024-03-11 10:42:10 -0600

    new update

--- funciones_ipc.R
+++ funciones_ipc.R
@@ -24,3 +24,38 @@ variacion <- function(x){
   data.frame(x %>% 
               mutate(var_prod = (precio_actual/cantidad_actual)*(cantidad_anterior/precio_anterior)))
 }
+
+
+
+#funcion para generar el excel con los indices
+exportarexcel <- function(archivo, ...){
+  indices <- createWorkbook() # se crea el workbook
+  
+  nombrehojas <- c("Ind_Producto", "Ind_Subclase", "Ind_Clase", "Ind_Grupo", 
+                   "Ind_Div", "Ind_Rep_Prod", "Ind_Rep_Subclase", "Ind_Rep_Clase", 
+                   "Ind_Rep_Grupo", "Ind_Rep_Div","Ind_Gen", "Ind_Rep", 
+                   "Incid_Prod", "Incid_Div", "Incid_Reg", "No_cotizados")
+  
+  for (i in 1:16) {
+    addWorksheet(indices, sheetName = nombrehojas[i])
+  }
+  
+  writeData(indices, sheet = 1, ind_prod01)
+  writeData(indices, sheet = 2, ind_subclase)
+  writeData(indices, sheet = 3, ind_clase)
+  writeData(indices, sheet = 4, ind_grupo)
+  writeData(indices, sheet = 5, ind_div)
+  writeData(indices, sheet = 6, ind_rep_prod)
+  writeData(indices, sheet = 7, ind_rep_subclase)
+  writeData(indices, sheet = 8, ind_rep_clase)
+  writeData(indices, sheet = 9, ind_rep_grupo)
+  writeData(indices, sheet = 10, ind_rep_div)
+  writeData(indices, sheet = 11, ind_gen)
+  writeData(indices, sheet = 12, ind_rep)
+  writeData(indices, sheet = 13, incid_prod)
+  writeData(indices, sheet = 14, incid_div)
+  writeData(indices, sheet = 15, incid_reg)
+  writeData(indices, sheet = 16, prod_faltantes)
+  
+  saveWorkbook(indices, archivo, overwrite = TRUE)
+}
