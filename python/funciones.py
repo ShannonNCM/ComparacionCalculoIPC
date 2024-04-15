558ad7248121e97c231366f694a7e9a7305bf99c
ShannonNCM <61364989+ShannonNCM@users.noreply.github.com>
2024-04-15 09:16:36 -0600

    update

--- python/funciones.py
+++ python/funciones.py
@@ -21,14 +21,41 @@ def cod_grupos(x):
     x['cod_div'] = x['cod_prod'].str.slice(stop=-5)
     return x
 
-#funcion para calcular los indices de cada grupo
-def indices(nd_prod, cod_subclase):
-    result = nd_prod[['region', cod_subclase, 'grupo_nombre', 'ponderacion_region', 'ind_prod']].rename(columns={cod_subclase: 'grupo_codigo'}).groupby(['region', 'grupo_codigo']).apply(lambda x: pd.Series({
+#funcion para calcular los indices de cada grupo por region
+def indices(x, y):
+    indice = x[['region', y, 'grupo_nombre', 'ponderacion_region', 'ind_prod']].rename(columns={y: 'grupo_codigo'}).groupby(['region', 'grupo_codigo']).apply(lambda x: pd.Series({
             'sum1': (x['ind_prod'] * x['ponderacion_region']).sum(),
             'sum2': x['ponderacion_region'].sum()
         })).reset_index()
 
-    result['ind'] = result['sum1'] / result['sum2']
-    result = result[['region', 'grupo_codigo', 'ind']]
+    indice['ind'] = indice['sum1'] / indice['sum2']
+    indice = indice[['region', 'grupo_codigo', 'ind']]
 
-    return result
+    return indice
+
+#funcion para calcular los indices de cada grupo a nivel republica
+def indicerep(df, y):
+    indice = df.groupby(y).apply(lambda x: pd.Series({
+        'sum1': (x['ponderacion_region'] * x['ind_prod']).sum(),
+        'sum2': x['ponderacion_region'].sum()
+    })).reset_index()
+
+    indice['ind_prod_rep'] = indice['sum1'] / indice['sum2']
+    indice.dropna(inplace=True)
+    indice = indice[[y, 'ind_prod_rep']]
+
+    return indice
+
+#funcion para calcular el indice por region a nivel nacional
+def indicenac(df):
+    indice = df.groupby('region').apply(lambda x: pd.Series({
+        'sum1': (x['ponderacion_region'] * x['ind_prod']).sum(),
+        'sum2': x['ponderacion_region'].sum()
+    })).reset_index()
+
+    indice['ind_prod_rep'] = indice['sum1'] / indice['sum2']
+    indice.dropna(inplace=True)
+    indice = indice[['region', 'ind_prod_rep']]
+
+    return indice
+
