d0d561251f6ed49afa50881b4118df1ba44c6d34
ShannonNCM <61364989+ShannonNCM@users.noreply.github.com>
2024-07-30 10:41:59 -0600

    update

--- calculoipcR_base2023/Resumen_Indices_2010_2023/funciones.R
+++ calculoipcR_base2023/Resumen_Indices_2010_2023/funciones.R
@@ -13,3 +13,16 @@ read_ind <- function(year, indices, sheet_names) {
   return(df_list)
 }
 
+
+#funcion para leer los archivos de excel con los factores estacionales
+read_est <- function(year, indices, sheet_names){
+  df_list <- vector("list", length(indices))
+  
+  for (i in indices) {
+    file_path <- paste0(year, "estacional/indxbase", year, "_factest_", i, "_2024.xlsx")
+    sheet_name <- sheet_names[i]
+    df_list[[i]] <- read_excel(file_path, sheet = sheet_name)
+  }
+  
+  return(df_list)
+}
